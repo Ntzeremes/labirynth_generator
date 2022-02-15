11b8e6ebee363c7eef2eb64ecdfc86eda1e4bb8f
Chris Ntzeremes <chrisntzerem@gmail.com>
2022-02-15 13:33:40 +0200

    Labyrinth class completed

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class Node:
     """A node is a part of the path. Each node is connected with the previous one, called parent.
     All the nodes connected create the path"""
 
+    propagate_chance = 0.5
+    parents_direction = 0.65
+
     def __init__(self, parent, pos):
         self.parent = parent
         self.pos_x = pos[0]
@@ -24,7 +27,7 @@ class Node:
                 if 0 <= x < labyrinth.size and 0 <= y < labyrinth.size:
                     if labyrinth.grid[x][y] is not None:
                         n += 1
-        return False if n > 1 else True
+        return True if n == 1 else False
 
     def neighbors(self, labyrinth):
         """checking the grid positions on the top, bottom, left and right of the Node.
@@ -38,34 +41,57 @@ class Node:
         # top
         if self.pos_y - 1 >= 0:
             if labyrinth.grid[self.pos_x][self.pos_y - 1] is None:
-                neighbors.append((self.pos_x, self.pos_y - 1))
+                n = 0
+                for i in range(2):
+                    for j in range(2):
+                        x = self.pos_x + i * (-1) ** j
+                        y = self.pos_y - 1 + (1 - i) * (-1) ** j
+                        if 0 <= x < labyrinth.size and 0 <= y < labyrinth.size:
+                            if labyrinth.grid[x][y] is not None:
+                                n += 1
+                if n == 1:
+                    neighbors.append((self.pos_x, self.pos_y - 1))
         # bottom
         if self.pos_y + 1 < labyrinth.size:
             if labyrinth.grid[self.pos_x][self.pos_y + 1] is None:
-                neighbors.append((self.pos_x, self.pos_y + 1))
+                n = 0
+                for i in range(2):
+                    for j in range(2):
+                        x = self.pos_x + i * (-1) ** j
+                        y = self.pos_y + 1 + (1 - i) * (-1) ** j
+                        if 0 <= x < labyrinth.size and 0 <= y < labyrinth.size:
+                            if labyrinth.grid[x][y] is not None:
+                                n += 1
+                if n == 1:
+                    neighbors.append((self.pos_x, self.pos_y + 1))
         # left
         if self.pos_x - 1 >= 0:
             if labyrinth.grid[self.pos_x - 1][self.pos_y] is None:
-                neighbors.append((self.pos_x - 1, self.pos_y))
+                n = 0
+                for i in range(2):
+                    for j in range(2):
+                        x = self.pos_x - 1 + i * (-1) ** j
+                        y = self.pos_y + (1 - i) * (-1) ** j
+                        if 0 <= x < labyrinth.size and 0 <= y < labyrinth.size:
+                            if labyrinth.grid[x][y] is not None:
+                                n += 1
+                if n == 1:
+                    neighbors.append((self.pos_x - 1, self.pos_y))
         # right
         if self.pos_x + 1 < labyrinth.size:
             if labyrinth.grid[self.pos_x + 1][self.pos_y] is None:
-                neighbors.append((self.pos_x + 1, self.pos_y))
+                n = 0
+                for i in range(2):
+                    for j in range(2):
+                        x = self.pos_x + 1 + i * (-1) ** j
+                        y = self.pos_y + (1 - i) * (-1) ** j
+                        if 0 <= x < labyrinth.size and 0 <= y < labyrinth.size:
+                            if labyrinth.grid[x][y] is not None:
+                                n += 1
+                if n == 1:
+                    neighbors.append((self.pos_x + 1, self.pos_y))
 
-        checked = copy.deepcopy(neighbors)
-        for neighbor in neighbors:
-            n = 0
-            for i in range(2):
-                for j in range(2):
-                    x = neighbor[0] + i * (-1) ** j
-                    y = neighbor[1] + (1 - i) * (-1) ** j
-                    if 0 <= x < labyrinth.size and 0 <= y < labyrinth.size:
-                        if labyrinth.grid[x][y] is not None:
-                            n += 1
-            if n > 1:
-                checked.remove(neighbor)
-
-        return checked
+        return neighbors
 
     def children(self, lab):
         """choosing the child node of the current node"""
@@ -74,15 +100,15 @@ class Node:
         if not neighbors:
             return None
         second_child = None
+
         # Giving the same direction coming from parent a higher chance
         if self.parent:
-            direction = [-self.parent[0] + self.pos_x, -self.parent[1] + self.pos_y]
 
             i = random.random()
             # keep parent's direction,it has a higher chance
-            parents_direction_node = (self.pos_x + direction[0], self.pos_y + direction[1])
+            parents_direction_node = (2 * self.pos_x - self.parent[0], 2 * self.pos_y - self.parent[1])
             if parents_direction_node in neighbors:
-                if len(neighbors) == 1 or i < 0.6:
+                if len(neighbors) == 1 or i < Node.parents_direction:
                     first_child = Node((self.pos_x, self.pos_y), parents_direction_node)
                 # else choose randomly between the other neighbors
                 else:
@@ -97,9 +123,9 @@ class Node:
 
         if l == 0:
             return None
-        elif len(neighbors) > 1:
+        elif l > 1:
             propagate = random.random()
-            if propagate < 0.15:
+            if propagate < Node.propagate_chance :
                 neighbors.remove((first_child.pos_x, first_child.pos_y))
                 second_child = Node((self.pos_x, self.pos_y), random.choice(neighbors))
 
@@ -121,12 +147,12 @@ class Labyrinth:
         self.block = None
 
     # noinspection PyTypeChecker
-    def insert(self, node):
+    def insert(self, x, y):
         """inserts a node in grid and prints it"""
-        self.grid[node.pos_x][node.pos_y] = 0
+        self.grid[x][y] = 0
         if self.screen:
             pygame.draw.rect(self.screen, (150, 150, 150),
-                             (node.pos_x * self.block + 1, node.pos_y * self.block + 1, self.block - 1, self.block - 1))
+                             (x * self.block + 1, y * self.block + 1, self.block - 1, self.block - 1))
 
     def print_grid(self):
         for i in range(self.size):
@@ -137,7 +163,7 @@ class Labyrinth:
         self.screen = screen
         self.block = block
 
-    def path(self):
+    def path(self, x, y):
         """This function generates a random path inside the grid.
         The path starts at a random point at the edge of a grid and has fixed length"""
 
@@ -145,21 +171,12 @@ class Labyrinth:
         side = random.choice([0, 1, 2, 3])  # 0 is left side, 1 is top etc
         position = random.randint(1, self.size - 1)
 
-        if side == 0:
-            start = (0, position)
-        elif side == 1:
-            start = (position, 0)
-        elif side == 2:
-            start = (self.size - 1, position)
-        else:
-            start = (position, self.size - 1)
-
-        current_nodes = [Node(None, start)]
-
+        current_nodes = [Node(None, (x, y))]
+        self.insert(current_nodes[0].pos_x, current_nodes[0].pos_y)
         while current_nodes:
             current_node = current_nodes.pop(0)
             if current_node.can_insert(self):
-                self.insert(current_node)
+                self.insert(current_node.pos_x, current_node.pos_y)
             children = current_node.children(self)
             if children:
                 current_nodes.extend(children)
@@ -183,23 +200,31 @@ def visualize_path_creation(screen_size):
         pygame.draw.line(screen, (0, 0, 0), (0, (i + 1) * block), (screen_size[0], (i + 1) * block))
 
     lab.set_visual_p(screen, block)
-    path = lab.path()
 
     pygame.display.flip()
     clock = pygame.time.Clock()
-
+    start = False
     while not terminate:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate = True
 
-        try:
-            next(path)
-        except StopIteration:
-            print("Labyrinth ready")
+            if not start:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x = int((event.pos[0]) // block)
+                    y = int((event.pos[1]) // block)
+                    path = lab.path(x, y)
+                    start = True
+
+        if start:
+            try:
+                next(path)
+            except StopIteration:
+                start = False
 
         pygame.display.flip()
 
 
 visualize_path_creation((800, 800))
+
