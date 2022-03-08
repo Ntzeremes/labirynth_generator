94e9b35295a136d957810401cd90625d0ee99652
Chris Ntzeremes <chrisntzerem@gmail.com>
2022-03-09 01:40:09 +0200

    Finished the starting gui

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def create_grid(screen, screen_width,screen_height, width, height, pad):
         pygame.draw.line(screen, (0, 0, 0), ((j + 1) * block, pad), ((j + 1) * block, screen_height + pad))
 
 
-def main(width, height):
+def main():
     """Initializes and visualizes the labyrinth using pygame and the classes from algorithm.py.
     """
 
@@ -29,34 +29,44 @@ def main(width, height):
     screen = pygame.display.set_mode((screen_width + right_pad, screen_height + top_pad))
     pygame.display.set_caption("Labyrinth Generator")
     terminate = False
-    lab = Labyrinth(width, height)
-    block = screen_width // width
-    lab.set_visual_p(screen, block, top_pad)
-    font = pygame.font.Font(None, 30)
-
-    create_grid(screen, screen_width, screen_height, width, height, top_pad)
-
+    pygame.font.init()
+    font = pygame.font.SysFont("arial", 40)
+    lab, width, height,block = None, None, None,None
     pygame.display.flip()
     clock = pygame.time.Clock()
     start_settings = False
     while not terminate:
         clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+
+        if start_settings:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate = True
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x = int((event.pos[0]) // block)
+                    y = int((event.pos[1] - top_pad) // block)
+                    lab.path(x, y)
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        lab.reset()
+                        create_grid(screen, screen_width, screen_height, width, height, top_pad)
+                        pygame.display.flip()
+
+        if not start_settings:
+            grid_choice = starting_gui(screen, font, screen_width, top_pad, right_pad)
+            if grid_choice:
+                width , height = grid_choice[0], grid_choice[1]
+                create_grid(screen, screen_width, screen_height, width, height, top_pad)
+                lab = Labyrinth(width, height)
+                block = screen_width // width
+                lab.set_visual_p(screen, block, top_pad)
+                pygame.display.flip()
+                start_settings = True
+            else:
                 terminate = True
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x = int((event.pos[0]) // block)
-                y = int((event.pos[1] - top_pad) // block)
-                lab.path(x, y)
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    lab.reset()
-                    create_grid(screen, screen_width, screen_height, width, height)
-                    pygame.display.flip()
-
-            pygame.display.flip()
-
-
-main(60, 40)
+if __name__ == "__main__":
+    main()
--- start_gui.py
+++ start_gui.py
@@ -2,6 +2,7 @@ import pygame
 
 
 class Text:
+    """Text class that prints text on the screen surface"""
     def __init__(self, font, pos, width, height, text):
         self.font = font
         self.top_rect = pygame.Rect(pos, (width, height))
@@ -20,7 +21,7 @@ class Button:
     """Creates the buttons for gui"""
     def __init__(self, text, width, height, pos, font):
         self.top_rect = pygame.Rect(pos, (width, height))
-        self.top_color = (200, 200, 200)
+        self.top_color = (160, 160, 160)
         self.pressed = False
 
         self.text_surf = font.render(text, True, (0, 0, 0))
@@ -33,47 +34,48 @@ class Button:
     def check_click(self):
         mouse_pos = pygame.mouse.get_pos()
         if self.top_rect.collidepoint(*mouse_pos):
+            self.top_color = (220, 220, 220)
             if pygame.mouse.get_pressed()[0]:
                 self.pressed = True
             else:
                 if self.pressed:
                     self.pressed = False
                     return True
+        else:
+            self.top_color = (160, 160, 160)
         return False
 
 
+# noinspection PyUnresolvedReferences
 def starting_gui(screen, font, width, top_pad, right_pad):
+    """Created the starting screen from which the user will choose the size of the labyrinth"""
     terminate = False
-    screen.fill((0, 0, 0))
-    page = 1
+    clock = pygame.time.Clock()
+
+    choice = None
+    grid_choices = {0: [None, "9 x 6", (9, 6)], 1: [None, "15 x 12", (15, 12)], 2: [None, "30 x 20", (30, 20)],
+                    3: [None, "60 x 40", (60, 40)]}
+    text = "Choose the labyrinth grid size."
+    page_text = Text(font, (100, 100), width - right_pad, 40, text)
+    button_h = 90
+    for i in range(len(grid_choices)):
+        grid_choices[i][0] = Button(grid_choices[i][1], 200, 50, (400, 210 + i * button_h), font)
+
     while not terminate:
+        screen.fill((0, 0, 0))
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate = True
 
-        if page == 1:
+        page_text.draw(screen)
 
-            grid_choices = {0: [None, "9 x 6"], 1: [None, "15 x 12"], 2: [None, "30 x 20"], 3: [None, "60 x 40"]}
-            text = "The labyrinth has a 3:2  width to height ratio."
-            textb = "Choose a grid size."
-            page_text = Text(font, (500, 50), width - right_pad, 40, text)
-            page_textb = Text(font, (500, 100), width - right_pad, 40, textb)
-            page_text.draw(screen)
-            page_textb.draw(screen)
+        for i in range(len(grid_choices)):
+            grid_choices[i][0].draw(screen)
+            if grid_choices[i][0].check_click():
+                choice = grid_choices[i][2]
+                terminate = True
 
-            buttons = False
-            button_h = 90
+        pygame.display.flip()
 
-            if not buttons:
-                for i in range(len(grid_choices)):
-                    grid_choices[i][0] = Button(grid_choices[i][1], 200, 50, (400, 210 + i * button_h), font)
-
-            for i in range(len(grid_choices)):
-                grid_choices[i][0].draw(screen)
-
-            pygame.display.flip()
-
-        else:
-            screen.fill((0, 0, 0))
-
-
+    return choice
